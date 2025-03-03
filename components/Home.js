1c0fd4ad35a69f9c322d6c0af3939f807dcb59b6
Quentin Pegna <quentin.pegna@gmail.com>
2025-03-03 09:00:47 +0100

    Home,project changés

--- components/Home.js
+++ components/Home.js
@@ -1,18 +1,48 @@
 import styles from '../styles/layout.module.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 
 export default function Home() {
-    
+    const router = useRouter();
+    const [projectsFromDB, setProjectsFromDB] = useState([]);
+
+    useEffect(() => {
+    fetch('http://localhost:3000/projects/all')
+    .then(response => response.json())
+    .then(data => {
+        data.result && setProjectsFromDB(data.projectsData)
+    })        
+
+    }, [])
+
+
+    const projects = projectsFromDB.map((data, i) => {
+        let totalContributed = 10000 // !!! remplacer par un map/boucle de data.progressions
+        let percent = totalContributed / data.goal * 100
+        return (
+            <div key={i} onClick={() => router.push(`/projects/${data.pageURL}`)}>
+                <h3>{data.title}</h3>
+                <p>{data.pitch}</p>
+                <div>
+                    <p>{percent} %</p>
+                    <p>{totalContributed}</p>
+                    <p>{data.goal}</p>
+                </div>
+            </div>
+        )
+    })
 
     return (
         <>
-            <h2>Top Projects</h2>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+            <h2>Discover our top projects</h2>
             <div>
-                <ProjectInfo />
-                <ProjectInfo />
-                <ProjectInfo />
-                <ProjectInfo />
+                {projects}
             </div>
         </>
     )
--- components/ProjectInfo.js
+++ components/ProjectInfo.js
@@ -1,14 +1,20 @@
 import styles from '../styles/project.module.css';
 import { useRouter } from 'next/router';
 
-export default function ProjectInfo() {
+export default function ProjectInfo(props) {
     const router = useRouter();
-
+    let totalContributed = 10000
+    let percent = totalContributed / props.goal * 100 
     //a créer, a pour but d'être un résumé du projet avec catégories et resumé peut etre une photo ?
     return (
-        <div className={styles.projectCard} onClick={() => router.push('/project')}>
-            <h3>Nom du Projet</h3>
-            <p>Description courte du projet...</p>
+        <div className={styles.projectCard} onClick={() => router.push(`/projects/${props.title}`)}>
+            <h3>{props.title}</h3>
+            <p>{props.pitch}</p>
+            <div>
+                <p>{percent}</p>
+                <p>{totalContributed}</p>
+                <p>{props.goal}</p>
+            </div>
         </div>
     );
 }
