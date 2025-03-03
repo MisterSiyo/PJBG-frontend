18f08901331cb83e9b711821e397e615bfdb2835
Theo Gosset <tagosset@gmail.com>
2025-03-03 17:30:26 +0100

    merge

--- components/Home.js
+++ components/Home.js
@@ -1,34 +1,36 @@
-import styles from "../styles/layout.module.css";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
+import styles from '../styles/layout.module.css';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 
 export default function Home() {
-  const router = useRouter();
-  const [projectsFromDB, setProjectsFromDB] = useState([]);
+    const router = useRouter();
+    const [projectsFromDB, setProjectsFromDB] = useState([]);
 
-  useEffect(() => {
-    fetch("http://localhost:3000/projects/all")
-      .then((response) => response.json())
-      .then((data) => {
-        data.result && setProjectsFromDB(data.projectsData);
-      });
-  }, []);
+    useEffect(() => {
+    fetch('http://localhost:3000/projects/all')
+    .then(response => response.json())
+    .then(data => {
+        data.result && setProjectsFromDB(data.projectsData)
+    })        
 
-  const projects = projectsFromDB.map((data, i) => {
-    let totalContributed = 10000; // !!! remplacer par un map/boucle de data.progressions
-    let percent = (totalContributed / data.goal) * 100;
-    return (
-      <div key={i} onClick={() => router.push(`/project/${data.pageURL}`)}>
-        <h3>{data.title}</h3>
-        <p>{data.pitch}</p>
-        <div>
-          <p>{percent} %</p>
-          <p>{totalContributed}</p>
-          <p>{data.goal}</p>
-        </div>
-      </div>
-    );
-  });
+    }, [])
+
+
+    const projects = projectsFromDB.map((data, i) => {
+        let totalContributed = 10000 // !!! remplacer par un map/boucle de data.progressions
+        let percent = totalContributed / data.goal * 100
+        return (
+            <div key={i} onClick={() => router.push(`/projects/${data.pageURL}`)}>
+                <h3>{data.title}</h3>
+                <p>{data.pitch}</p>
+                <div>
+                    <p>{percent} %</p>
+                    <p>{totalContributed}</p>
+                    <p>{data.goal}</p>
+                </div>
+            </div>
+        )
+    })
 
     return (
         <>
@@ -38,8 +40,6 @@ export default function Home() {
         <br/>
         <br/>
         <br/>
-            <h2>Want to create your own ?</h2>
-            <button onClick={() => router.push(`/gameCreation`)}>Create your project</button>
             <h2>Discover our top projects</h2>
             <div>
                 {projects}
